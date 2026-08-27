4e68e583ef94245f1ff9f9879d907d19a88bec91
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2026-08-27 09:48:52 -0400

    Initial Commit

---
+++ BIgVTests/BIgVTests.swift
@@ -0,0 +1,19 @@
+//
+//  BIgVTests.swift
+//  BIgVTests
+//
+//  Created by Gp. on 8/27/26.
+//
+
+import Testing
+@testable import BIgV
+
+struct BIgVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
